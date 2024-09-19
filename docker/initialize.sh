09c6ba828e3359343650eb7b4de7bc3958f8395e
Ammonknows <jangkorzeniewski@gmail.com>
2024-09-20 00:02:13 +0200

    Update initialize.sh

--- docker/initialize.sh
+++ docker/initialize.sh
@@ -3,16 +3,14 @@
 # Ensure .bashrc is in the root directory
 if [ ! -f /root/.bashrc ]; then
     cp /etc/skel/.bashrc /root/.bashrc
-    chmod 444 /root/.bashrc
+    chmod 777 /root/.bashrc
 fi
 
 # Ensure .profile is in the root directory
 if [ ! -f /root/.profile ]; then
-    cp /etc/skel/.bashrc /root/.profile
-    chmod 444 /root/.profile
+    cp /etc/skel/.profile /root/.profile
+    chmod 777 /root/.profile
 fi
-
-apt-get update
 
 # Start SSH service
 exec /usr/sbin/sshd -D
